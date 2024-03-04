431c9f9cfc40397c4a1c05600d832ab36b087847
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-03-04 10:05:58 +0400

    added new file

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var number=prompt('Enter number:')
+var numberInt=parseInt(number)
+
+if (number %2==0) {
+    alert( (number)+' '+'is an even number')
+}
+else{
+    alert( (number)+' '+'is an odd number')
+}
